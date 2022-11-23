ec98210f6e9b3a2fa41dd76980d8defe2595d41d
Mike Andrade <hoodloom07@outlook.es>
2022-11-23 01:17:12 -0600

    2. how work react and comments about this

--- miapp/src/index.js
+++ miapp/src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/**
+ * la variable root es para darle referencia a su contra parte en html, donde hay un div con id...
+ * root, este contendra todo lo que react necesite poner o anexar
+ * 
+ * reactDOOM -> hace referenicia al uso del doom dentro de la pagina web
+ */
+root.render(<React.StrictMode> <App /></React.StrictMode>);
+/**
+ * streactMode que vamos a renderizar en modo estricto la aplicacion
+ * ¿ Que aplicacion? pues la function app que que esta importada desde la parte inicial del script
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
